321b32cf4a20029b99b73e15bf288d241bffe4c8
batsdo <102225724+batsdo@users.noreply.github.com>
2024-06-20 12:47:13 +0200

    Update complete.py

--- complete.py
+++ complete.py
@@ -3,6 +3,8 @@ from pythonosc import osc_server
 from djitellopy import Tello
 from pynput import keyboard
 
+dummy1 = 0
+dummy2 = 0
 
 tello = Tello()
 tello.connect()
@@ -30,10 +32,16 @@ def move_right():
     tello.send_rc_control(speed, 0, 0, 0)
 
 def yaw_left():
-    tello.send_rc_control(0,0,0,-speed)
+    tello.send_rc_control(0, 0, 0, -speed)
 
 def yaw_right():
-    tello.send_rc_control(0,0,0,speed)
+    tello.send_rc_control(0, 0, 0, speed)
+
+def move_up():
+    tello.send_rc_control(0, 0, speed, 0)
+
+def move_down():
+    tello.send_rc_control(0, 0, -speed, 0)
 
 def stop():
     tello.send_rc_control(0, 0, 0, 0)
@@ -46,41 +54,61 @@ def quaternion_handler(address, *args):
         second_value = args[1]
         global third_value
         third_value = args[2]
-        print(f"First value: {first_value}, Second value: {second_value}")
-        if first_value < -0.3:
+        global fourth_value
+        fourth_value = args[3]
+        print(f"w: {first_value}, x: {second_value}, y: {third_value}, z: {fourth_value}")
+        if second_value < -0.2:
             move_forward()
             print("Vorwärts")
-        elif first_value > 0.3:
+        elif second_value > 0.2:
             move_backward()
             print("Rückwärts")
-        elif second_value < -0.3:
+        elif third_value < -0.2:
             move_left()
             print("Links")
-        elif second_value > 0.3:
+        elif third_value > 0.2:
             move_right()
             print("Rechts")
-        elif third_value > 0.2:
+        elif fourth_value > 0.2:
             yaw_left()
             print("Links drehen")
-        elif third_value < -0.2:
+        elif fourth_value < -0.2:
             yaw_right()
             print("Rechts drehen")
+        elif dummy1 == 1:
+            move_up()
+            print("Flying up")
+        elif dummy2 == 1:
+            move_down()
+            print("Flying down")
         else:
             stop()
+
     else:
         print("Received quaternion message with insufficient arguments")
 
-def touchcount_handler(address, *args):
-    global touch_count
+def button_handler(address, *args):
+    global number, value, dummy1, dummy2
     if len(args) >= 1:
-        touch_count = args[0]
-        print(f"Touch count: {touch_count}")
-        if touch_count == 2:
+        number = args[0]
+        value = args[1]
+
+        print(f"Button: {number}, Value: {value}")
+        if number == 1 and value == 1:
             takeoff()
             print("Takeoff")
-        elif touch_count == 1:
+        elif number == 3 and value == 1:
             land()
             print("Landing")
+        elif number == 7 and value == 1:
+            dummy1 = 1
+        elif number == 9 and value == 1:
+            dummy2 = 1
+        elif number == 7 and value == 0:
+            dummy1 = 0
+        elif number == 9 and value == 0:
+            dummy2 = 0
+
     else:
         print("Received touch count message with insufficient arguments")
 
@@ -88,8 +116,8 @@ def touchcount_handler(address, *args):
 if __name__ == "__main__":
     # Dispatcher to handle incoming OSC messages
     dispatcher = dispatcher.Dispatcher()
-    dispatcher.map("/ZIGSIM/*/quaternion", quaternion_handler)
-    dispatcher.map("/ZIGSIM/*/touchcount", touchcount_handler)
+    dispatcher.map("/gyrosc/quat", quaternion_handler)
+    dispatcher.map("/gyrosc/button", button_handler)
 
     # Define the IP address and port for the OSC server
     ip = "0.0.0.0"  # Listen on all available interfaces
